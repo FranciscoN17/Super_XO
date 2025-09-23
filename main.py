9d08808b0a20a5a8c80870d4b1f3f21e551e3fea
Francisco Nazário <francisconazario0136@gmail.com>
2025-09-23 16:39:00 -0300

    creates first flat screen

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello World")
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Super XO")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+    
+    screen.fill((255,255,255))
+
+    pygame.display.flip()
+
+pygame.quit()
