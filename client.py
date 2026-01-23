27c230076e507259e529c7d61fc31231057f4a54
Francisco Nazário <francisconazario0136@gmail.com>
2026-01-23 19:45:09 -0300

    first working version

--- client.py
+++ client.py
@@ -1,8 +1,59 @@
-import socket
+import pygame, socket, pickle, threading, sys
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('localhost', 8080))
-client.send("I am a CLIENT\n".encode())
-from_server = client.recv(4096).decode()
-client.close()
-print(from_server)
+import utils, assets
+from utils import Board, Game
+
+
+SERVER_IP = "localhost"
+PORT = 5000
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((SERVER_IP, PORT))
+
+game = Game({(0,0): Board(assets.board_tables[0][0]), (0,1): Board(assets.board_tables[0][1]), (0,2): Board(assets.board_tables[0][2]), 
+             (1,0): Board(assets.board_tables[1][0]), (1,1): Board(assets.board_tables[1][1]), (1,2): Board(assets.board_tables[1][2]), 
+             (2,0): Board(assets.board_tables[2][0]), (2,1): Board(assets.board_tables[2][1]), (2,2): Board(assets.board_tables[2][2])
+             })
+
+def receive(socket):
+    global game
+    buffer = b""
+    while True:
+        try:
+            packet = socket.recv(4096)
+            if not packet:
+                break
+            buffer += packet
+            while buffer:
+                game = pickle.loads(buffer)
+                print("game recebido")
+                buffer = b""
+        except:
+            break
+
+threading.Thread(target=receive, args=(sock,), daemon=True).start()
+
+## GAME CONFIGURATION ##
+
+pygame.init()
+
+screen = pygame.display.set_mode((assets.height,assets.width))
+pygame.display.set_caption("Super XO - Client")
+
+while True:
+    utils.show_game(game, screen)
+    #EVENT HANDLER
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sock.close()
+            sys.exit()
+    
+    if utils.handle_click(event):
+        clicked_pos = utils.handle_click(event)
+        #Verifica em qual célula do tabuleiro o jogador clicou
+        board_key, board_turn  = utils.get_board_key_from_pos_global(game, clicked_pos)
+        data = pickle.dumps((board_key, board_turn))
+        sock.sendall(data)
+
+    pygame.display.flip()
--- server.py
+++ server.py
@@ -1,24 +1,77 @@
-import socket
+import socket, sys, threading
 import pickle
+import pygame
 
-server = "localhost"
-port = 8080
+import utils
+import assets
+from utils import Board, Game
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST = "localhost"
+PORT = 5000
 
-try:
-    s.bind((server, port))
-except socket.error as e:
-    print(str(e))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen(2)
 
-s.listen(2)
-print("Waiting for a connection, server started")
+clients = []
+symbols = ["X", "O"]
 
-connected = set()
-games = {}
-idCount = 0
+print("Servidor aguardando jogadores...")
 
-def threaded_client(conn, p, gameId):
-    global idCount
-    conn.send(str.encode(str(p)))
-    
+game = Game({(0,0): Board(assets.board_tables[0][0]), (0,1): Board(assets.board_tables[0][1]), (0,2): Board(assets.board_tables[0][2]), 
+             (1,0): Board(assets.board_tables[1][0]), (1,1): Board(assets.board_tables[1][1]), (1,2): Board(assets.board_tables[1][2]), 
+             (2,0): Board(assets.board_tables[2][0]), (2,1): Board(assets.board_tables[2][1]), (2,2): Board(assets.board_tables[2][2])
+             })
+
+def receive(socket, symbol):
+    global game
+    buffer = b""
+    while True:
+        if symbol == game.player_turn:
+            try:
+                packet = socket.recv(4096)
+                if not packet:
+                    break
+                buffer += packet
+                while buffer:
+                    (board_key_temp, board_turn_temp) = pickle.loads(buffer)
+
+                    if board_key_temp and board_turn_temp:
+                        if (board_turn_temp == game.board_turn or game.free_play):
+                            if not game.info[board_turn_temp].info[board_key_temp] and not game.info[board_turn_temp].winner:
+                                game = utils.make_move(game, board_turn_temp, board_key_temp)
+                                data = pickle.dumps(game)
+                                for client in clients:
+                                    client.sendall(data)
+                    buffer = b""
+
+            except:
+                break
+
+while len(clients) < 2:
+    client, addr = server.accept()
+    symbol = symbols[len(clients)]
+    clients.append(client)
+    threading.Thread(target=receive, args=(client, symbol), daemon=True).start()
+    print(f"Jogador {symbol} conectado:", addr)
+
+## GAME CONFIGURATION ##
+
+pygame.init()
+
+screen = pygame.display.set_mode((assets.height,assets.width))
+pygame.display.set_caption("Super XO - Server")
+
+
+while True:
+    utils.show_game(game, screen)
+    #EVENT HANDLER
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            server.close()
+            sys.exit()
+
+    pygame.display.flip()
+
+        
