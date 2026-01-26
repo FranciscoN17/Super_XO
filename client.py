6a8c8966b542fefb5d423e3104a3ffa19f9cdd0d
Francisco Nazário <francisconazario0136@gmail.com>
2026-01-25 22:19:28 -0300

    adds Client class

--- client.py
+++ client.py
@@ -1,51 +1,7 @@
-import pygame, socket, pickle, threading, sys
+from network import Client
 
-import utils, assets, network
-from utils import Game
+client = Client()
 
-SERVER_IP = "192.168.0.154"
-PORT = 5000
+client.connect()
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((SERVER_IP, PORT))
-
-game = Game()
-my_symbol = ""
-
-def receive(socket):
-    global game, my_symbol
-    while True:
-        try:
-            msg = network.recv_msg(socket)
-            if not msg:
-                break
-            game, my_symbol = msg
-            print("game recebido")
-        except:
-            break
-
-threading.Thread(target=receive, args=(sock,), daemon=True).start()
-
-## GAME CONFIGURATION ##
-
-pygame.init()
-
-screen = pygame.display.set_mode((assets.height,assets.width))
-
-while True:
-    utils.show_game(game, screen)
-    pygame.display.set_caption(f"Super XO - Client {my_symbol}")
-    #EVENT HANDLER
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sock.close()
-            sys.exit()
-    
-    if utils.handle_click(event):
-        clicked_pos = utils.handle_click(event)
-        #Verifica em qual célula do tabuleiro o jogador clicou
-        board_key, board_turn  = utils.get_board_key_from_pos_global(game, clicked_pos)
-        network.send_msg(sock, (board_key, board_turn))
-
-    pygame.display.flip()
+client.start()
--- network.py
+++ network.py
@@ -1,16 +1,6 @@
-import pickle
-import utils
+import pickle, threading, socket, pygame, sys
+import utils, front, assets
 from utils import Game
-
-def send_game_state(game: Game, client_list, symbols = utils.symbols):
-    for i in range(len(client_list)):
-        data = pickle.dumps((game, symbols[i]))
-        client_list[i].sendall(data)
-
-def send_move(socket, board_key, board_turn):
-    data = pickle.dumps((board_key, board_turn))
-    socket.sendall(data)
-
 
 def send_msg(sock, obj):
     payload = pickle.dumps(obj)
@@ -34,3 +24,97 @@ def recv_msg(sock):
         data += packet
 
     return pickle.loads(data)
+
+def broadcast_msg(clients: list, obj):
+    for client in clients:
+        send_msg(client, (obj, utils.symbols[clients.index(client)]))
+
+class Server:
+    def __init__(self, host="localhost", port=5000, game=Game()):
+        self.host = host
+        self.port = port
+        self.clients = []
+        self.game = game
+
+    def handle_client(self, socket, symbol):
+        while True:
+            if symbol == self.game.player_turn:
+                try:
+                    msg = recv_msg(socket)
+                    if not msg:
+                        break
+                    (board_key_temp, board_turn_temp) = msg
+                    print("recebido:", (board_key_temp, board_turn_temp))
+
+                    if self.game.make_move(board_turn_temp, board_key_temp):
+                        broadcast_msg(self.clients, self.game)
+                        print("jogada feita:", (board_key_temp, board_turn_temp))
+                        
+                except:
+                    break
+
+    def start(self):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((self.host, self.port))
+        server.listen(2)
+
+        print("Servidor aguardando jogadores...")
+
+        while len(self.clients) < 2:
+            client, addr = server.accept()
+            symbol = utils.symbols[len(self.clients)]
+            self.clients.append(client)
+            threading.Thread(target=self.handle_client, args=(client, symbol), daemon=True).start()
+            print(f"Jogador {symbol} conectado:", addr)
+
+        broadcast_msg(self.clients, self.game)
+
+        front.run(self.game)
+
+        server.close()
+
+class Client:
+    def __init__(self, game = Game()):
+        self.symbol = ""
+        self.game = game
+        self.sock = None
+
+    def connect(self, server_ip = "localhost", port=5000):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((server_ip, port))
+    
+    def receive(self):
+        while True:
+            try:
+                msg = recv_msg(self.sock)
+                if not msg:
+                    break
+                self.game, self.symbol = msg
+                print("game recebido")
+            except:
+                break
+    
+    def start(self):
+        threading.Thread(target=self.receive, args=(), daemon=True).start()
+
+        pygame.init()
+        screen = pygame.display.set_mode((assets.height,assets.width))
+
+        while True:
+            front.show_game(self.game, screen)
+            pygame.display.set_caption(f"Super XO - Client {self.symbol}")
+            #EVENT HANDLER
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    self.sock.close()
+                    sys.exit()
+
+            if utils.handle_click(event):
+                clicked_pos = utils.handle_click(event)
+                #Verifica em qual célula do tabuleiro o jogador clicou
+                board_key, board_turn  = utils.get_board_key_from_pos_global(self.game, clicked_pos)
+                send_msg(self.sock, (board_key, board_turn))
+                print("enviado:", (board_key, board_turn))
+
+            pygame.display.flip()
